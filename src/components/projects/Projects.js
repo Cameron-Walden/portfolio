b41f21213034e365fb6db825fe85d19e7cbe9693
Cameron <cameronkwalden@gmail.com>
2022-02-12 19:03:09 -0800

    slight refactoring of mapping

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import ProjectDetailsModal from "../projectDetailsModal/ProjectDetailsModal";
-import './Projects.scss';
+import "./Projects.scss";
 
 export default function Projects(props) {
   const [projectDescription, setProjectDescription] = useState({});
   const [detailsModalShow, setDetailsModalShow] = useState(false);
 
-  const showModalDetails = data => {
+  const showModalDetails = (data) => {
     setDetailsModalShow({ detailsModalShow });
     setProjectDescription(data);
   };
@@ -17,35 +17,33 @@ export default function Projects(props) {
 
   if (props.resumeProjects && props.resumeBasicInfo) {
     var sectionName = props.resumeBasicInfo.section_name.projects;
-    var projects = props.resumeProjects.map(function (projects) {
-      return (
-        <div
-          className="col-sm-12 col-md-6 col-lg-4"
-          key={projects.title}
-          style={{ cursor: "pointer" }}
-        >
-          <span className="portfolio-item d-block">
-            <div className="foto" onClick={() => showModalDetails(projects)}>
-              <div>
-                <img
-                  src={projects.images[0]}
-                  alt="projectImages"
-                  height="230"
-                  style={{
-                    marginBottom: 0,
-                    paddingBottom: 0,
-                    position: "relative",
-                  }}
-                />
-                <span className="project-date">{projects.startDate}</span>
-                <br />
-                <p className="project-title-settings mt-3">{projects.title}</p>
-              </div>
+    var projects = props.resumeProjects.map((project) => (
+      <div
+        className="col-sm-12 col-md-6 col-lg-4"
+        key={project.title}
+        style={{ cursor: "pointer" }}
+      >
+        <span className="portfolio-item d-block">
+          <div className="foto" onClick={() => showModalDetails(project)}>
+            <div>
+              <img
+                src={project.images[0]}
+                alt="projectImages"
+                height="230"
+                style={{
+                  marginBottom: 0,
+                  paddingBottom: 0,
+                  position: "relative",
+                }}
+              />
+              <span className="project-date">{project.startDate}</span>
+              <br />
+              <p className="project-title-settings mt-3">{project.title}</p>
             </div>
-          </span>
-        </div>
-      );
-    });
+          </div>
+        </span>
+      </div>
+    ));
   }
 
   return (
