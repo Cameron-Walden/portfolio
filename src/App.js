ec006c9483a0baa9c5e81a8e51ccfd229e15a8ac
Cameron <cameronkwalden@gmail.com>
2022-02-12 19:06:35 -0800

    stashing before pushing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { useState } from "react";
+// import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header/Header";
@@ -27,7 +27,6 @@ class App extends Component {
     const resumePath = `projectsAndSkills.json`;
     this.loadResumeFromPath(resumePath);
   }
-
 
   loadResumeFromPath = path => {
     axios(path)
