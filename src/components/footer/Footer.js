15c076f6552dee7c11efe3e2881037926da59300
Cameron <cameronkwalden@gmail.com>
2022-02-14 13:57:48 -0800

    removes links from footer, adds link and animation to header

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,23 +2,21 @@ import './Footer.scss'
 
 export default function Footer(props){
 
-    if (props.sharedBasicInfo) {
-      var networks = props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
+    // if (props.sharedBasicInfo) {
+    //   var networks = props.sharedBasicInfo.social.map(network => (
+    //       <span key={network.name} className="m-4">
+    //         <a href={network.url} target="_blank" rel="noopener noreferrer">
+    //           <i className={network.class}></i>
+    //         </a>
+    //       </span>
+    //   ));
+    // }
 
     return (
       <footer>
         <div className="container">
           <div className="row">
-            <div className="col social-links">{networks}</div>
+            {/* <div className="col social-links">{networks}</div> */}
             <div className="col">
             </div>
             <div className="col">
