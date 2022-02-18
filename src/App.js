74552fd3872d6f552ce3f1992c810d1d3ca4e6aa
Cameron <cameronkwalden@gmail.com>
2022-02-18 15:26:54 -0800

    adds props to super

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import axios from "axios";
 class App extends Component {
 
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       resumeData: {},
       sharedData: {},
