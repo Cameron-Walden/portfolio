ee370cc3410e03fc50a6837704e7200ed79d2d08
Cameron <cameronkwalden@gmail.com>
2022-10-21 13:47:34 -0700

    destructures props

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,20 +2,20 @@ import Projects from "../projects/Projects";
 import Skills from "../skills/Skills";
 import Experience from "../experience/Experience";
 
-export default function Home(props) {
+export default function Home({ resumeData, sharedData }) {
     return (
       <div>
         <Projects
-          resumeProjects={props.resumeData.projects}
-          resumeBasicInfo={props.resumeData.basic_info}
+          resumeProjects={resumeData.projects}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Skills
-          sharedSkills={props.sharedData.skills}
-          resumeBasicInfo={props.resumeData.basic_info}
+          sharedSkills={sharedData.skills}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Experience
-          resumeExperience={props.resumeData.experience}
-          resumeBasicInfo={props.resumeData.basic_info}
+          resumeExperience={resumeData.experience}
+          resumeBasicInfo={resumeData.basic_info}
         />
       </div>
     );
