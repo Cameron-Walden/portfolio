137a497e7a38335b812120bb7f6722dab254dee5
Cameron <cameronkwalden@gmail.com>
2022-12-19 19:44:37 -0800

    fixes footer icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,39 +1,25 @@
-import './Footer.scss'
+import "./Footer.scss";
 
-export default function Footer(props){
+export default function Footer({ sharedBasicInfo }) {
 
-    // if (props.sharedBasicInfo) {
-    //   var networks = props.sharedBasicInfo.social.map(network => (
-    //       <span key={network.name} className="m-4">
-    //         <a href={network.url} target="_blank" rel="noopener noreferrer">
-    //           <i className={network.class}></i>
-    //         </a>
-    //       </span>
-    //   ));
-    // }
-
-    return (
-      <footer>
-        <div className="container">
-          <div className="row">
-            {/* <div className="col social-links">{networks}</div> */}
-            <div className="col">
-            </div>
-            <div className="col">
-              <div className="copyright py-4 text-center">
-                <div className="container">
-                  {/* <small>
-                    Copyright &copy;{" "}
-                    {props.sharedBasicInfo
-                      ? props.sharedBasicInfo.name
-                      : "???"}
-                  </small> */}
-                </div>
-              </div>
-            </div>
+  return (
+    <footer>
+        <div className="row">
+          <div className="socials"> 
+            {sharedBasicInfo?.social.map((network) => (
+              <span key={network.name} className="m-4">
+                  <a
+                    href={network.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="network-url"
+                  >
+                    <i className={network.class}></i>
+                  </a>
+              </span>
+            ))}
           </div>
         </div>
-      </footer>
-    );
+    </footer>
+  );
 }
-
