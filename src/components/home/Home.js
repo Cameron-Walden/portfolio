b63a91afc38a483b49f26d7b63d3c5859ad7833d
Cameron <cameronkwalden@gmail.com>
2023-03-02 16:57:15 -0800

    moves functions from App to Home, adds PUBLIC_URL to access

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,42 @@
+import axios from 'axios';
+import { PortfolioContext } from '../../context/PortfolioContext';
+import { useContext, useEffect } from 'react';
 import Skills from "../skills/Skills";
 
 export default function Home() {
+  const { setResumeData, setSharedData } = useContext(PortfolioContext)
+
+  const getProjectsAndSkills = () => {
+    const resumePath = `${process.env.PUBLIC_URL}/projectsAndSkills.json`;
+    loadResumeFromPath(resumePath);
+  };
+
+  const loadResumeFromPath = (path) => {
+    axios(path)
+      .then((response) => {
+        setResumeData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const loadPortfolioData = () => {
+    const url = `${process.env.PUBLIC_URL}/portfolioData.json`;
+
+    axios(url)
+      .then((response) => {
+        setSharedData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    loadPortfolioData();
+    getProjectsAndSkills();
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
       <Skills />
